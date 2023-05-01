f24bbe2aacbf467dacf94e51d7b23d7785846a1e
Vladislav Razyhraiev <nicorvenius@gmail.com>
2023-05-01 12:43:45 +0300

    fix(App): Fix prod enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
 export const environment = {
-    production: true,
-    apiURL: 'https://api.diststudy.dev/api/',
-    socketIoUrl: 'https://api.diststudy.dev/chat'
+  production: true,
+  apiURL: 'https://e-commerce-5jch.onrender.com/api/',
+  // apiURL: 'http://localhost:3000/api/',
+  // socketIoUrl: 'https://api.diststudy.dev/chat'
+  socketIoUrl: 'http://localhost:3000/chat',g
+  AUTH_API_VERSION: 1,
+  appPurchaseUrl: '',
+  appThemeName: 'ecommerce',
+  appDemos: '',
+  appPreviewUrl: '',
+  appVersion: '0'
 };
